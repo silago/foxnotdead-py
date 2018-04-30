ba60206919c9a6ad19fae6e88b5d72abf42acd2b
silago <silago.nevermind@gmail.com>
2018-05-01 00:50:20 +0300

    Procfile

--- foxnotdead/connection.py
+++ foxnotdead/connection.py
@@ -1,14 +1,14 @@
 from peewee import *
 import os
 
-db_type = os.environ["db_type"]
-db_name = os.environ["db_name"]
-db_user = os.environ["db_user"]
-db_pass = os.environ["db_pass"]
-db_host = os.environ["db_host"]
-db_port = os.environ["db_port"]
+db_type = os.environ.get("db_type","")
+db_name = os.environ.get("db_name","")
+db_user = os.environ.get("db_user","")
+db_pass = os.environ.get("db_pass","")
+db_host = os.environ.get("db_host","")
+db_port = os.environ.get("db_port","")
 
-
+print(db_type)
 if (db_type == "pg"):
     DATABASE = PostgresqlDatabase(db_name, user=db_user, password=db_pass,
                            host=db_host, port=db_port)
